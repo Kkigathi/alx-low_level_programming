8877ac31dcb0d93098b9d18d899ab51684254e44
Kkigathi <kigathi.g.james@gmail.com>
2022-07-02 04:53:30 -0700

    if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,15 +9,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	 your code goes there
 	return (0);
-}*/
+}
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
-   * main - Entry point
-    *
-     * Return: Always 0 (Success)
-      */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
