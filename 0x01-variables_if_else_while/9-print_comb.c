923e3f34cb31a5026a84b1eb27882c573fbc634d
Kkigathi <kigathi.g.james@gmail.com>
2022-07-03 02:29:01 -0700

    lower case print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	int d;
+
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 		if (d != '9')
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
