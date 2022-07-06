b61ed9662dd2ccce6063b037a5015771461a37a3
Kkigathi <kigathi.g.james@gmail.com>
2022-07-06 05:17:38 -0700

    4-isalppha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Check lower
+ * @c: An input character
+ * Description: function uses _putchar function to check
+ * alphabet in lowercase
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _islower(int c)
+{
+   char c;
+
+   if (((c>96) && (c<123))|| ((c>64) && (c<91)))
+   {
+         return (1);   
+   }     
+    else
+   {
+         return(0);
+   }
+}
