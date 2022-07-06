9ee52f514c8bcaef7a7c4881bae09e8669cfd641
Kkigathi <kigathi.g.james@gmail.com>
2022-07-06 05:52:44 -0700

    3-is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,13 +9,13 @@
  */
 
 int _islower(int c)
-{	
-	if (c>96 && c<123)
-   	{
-         return (1);   
+{
+	if (c > 96 && c < 123)
+	{
+	return (1);
 	}     
 	else
 	{
-         return(0);
+	return (0);
 	}
 }
