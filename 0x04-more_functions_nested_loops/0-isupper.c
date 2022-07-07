a943cb77425612fb9bc7fafe4ac7a33ac180155a
Kkigathi <kigathi.g.james@gmail.com>
2022-07-07 10:27:49 -0700

    0- isuppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #inlcude "main.h"
 /**
-  * int_isupper(int c) - checks for an upper case letter
+  *_isupper - checks for an upper case letter
   * @c: is an i/p xter
   * it then returns 1 if c is uppercase
   * Return: 0 otherwise
   */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if ((c > 64) && (c < 91))
 		return (1);
