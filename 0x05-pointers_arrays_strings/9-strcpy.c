26c328d6cf4916ad7a9b7a95f84dba5554679eeb
Kkigathi <kigathi.g.james@gmail.com>
2022-07-12 14:43:44 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies string pointed to by src
+ * @dest: dest to copy to
+ * @src: copy from address
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
