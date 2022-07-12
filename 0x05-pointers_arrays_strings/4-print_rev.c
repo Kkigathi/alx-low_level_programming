332aafccf5f72705bf45360d3247c0f34a39499d
Kkigathi <kigathi.g.james@gmail.com>
2022-07-12 12:55:22 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a str in reverse
+ * followed by a new line
+ * @s: string input
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int ln = 0;
+
+	while (s[ln] != '\0')
+	{
+		_putchar(s[ln]);
+		ln++;
+	}
+	while (len)
+		_putchar (s[--len]);
+
+	_putchar('\n');
+}
