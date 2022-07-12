802fbac139a82809ca592c0380c7cc12670f026e
Kkigathi <kigathi.g.james@gmail.com>
2022-07-12 13:41:50 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character 
+ * of a string starting from first
+ * followed by new line
+ * @str : input string
+ * Return : nothing
+ */
+
+void puts2(char *str)
+{
+	int ln = 0
+
+	while (str[ln] != '\0')
+	{
+		ln =+ 2;
+		_putchar (str[ln]);
+	}
+	_putchar ('\n');
+}
+
+
