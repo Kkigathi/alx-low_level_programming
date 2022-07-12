0c8e116d83eff858a34e331c3478dcc96ce3f6b5
Kkigathi <kigathi.g.james@gmail.com>
2022-07-12 14:24:10 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,17 @@
  * @str : input string
  * Return : nothing
  */
-
 void puts2(char *str)
 {
-	int ln = 0;
+	int ln = 0, i = 0;
 
 	while (str[ln] != '\0')
-	{
-		_putchar (str[0, ln += 2]);
-	}
-	_putchar ('\n');
+		ln++;
+
+	ln -= 1;
+
+	for (; i <= ln; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
-
-
