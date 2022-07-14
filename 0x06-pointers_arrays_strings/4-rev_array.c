ff6c6bacb99bb1e4df73ebaab14625bb44fce02a
Kkigathi <kigathi.g.james@gmail.com>
2022-07-14 11:33:56 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "mian.h"
+
+/**
+ * reverse_array - function that reverse the int in arrays
+ * @a: an input array
+ * @n: elements of the array
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int rev;
+
+	while (i < n--)
+	{
+		rev = a[i];
+		a[i++] = a[n];
+		a[n] = rev;
+	}
+}
