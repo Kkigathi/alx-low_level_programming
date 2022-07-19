644bfb4f0472e1cc95cd58d3400254d3d15ed1ec
Kkigathi <kigathi.g.james@gmail.com>
2022-07-19 14:20:44 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i; // i is the index of chr in the str
-	for (i=0; s[i] >= '\0'; i++)
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return(s + i);
+			return (s + i);
 	}
+
 	return (NULL);
 }
-
