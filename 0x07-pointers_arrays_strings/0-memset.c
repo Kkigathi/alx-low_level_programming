363a854b2438295a87030ff45c9927bddff024c0
Kkigathi <kigathi.g.james@gmail.com>
2022-07-19 13:42:03 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = b;
+	char *start = s;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (start);
 }
