d6f0c35d3d5efbc13018b537f138c7fa6525ea92
Kkigathi <kigathi.g.james@gmail.com>
2022-08-03 13:34:43 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+   * op_add - addition operator
+    * @a: first int
+     * @b: second int
+      * Return: sum of 2 numbers
+       */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
