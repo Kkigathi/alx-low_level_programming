b68ad6c804fc6a63a3187c29da9db2df0d3bbd94
Kkigathi <kigathi.g.james@gmail.com>
2022-08-15 11:12:12 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * free_listint2 - Frees a listint_t list.
  * @head: A pointer to the address of the
@@ -6,20 +7,19 @@
  *
  * Description: Sets the head to NULL.
  */
-
-
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
 	if (head == NULL)
 		return;
+
 	while (*head)
 	{
-		tmp = *(head)->next;
+		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
-
 	}
+
 	head = NULL;
 }
